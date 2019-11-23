248b67d887bab0e8b5b93a3d7d1df748fa2a2ae6
gainstrive <gainstrive@gmail.com>
2019-11-23 12:18:56 -0500

    Added new files for gameplay.

---
+++ public/js/questions.js
@@ -0,0 +1,162 @@
+var questions = {
+    category: {
+        animals: {
+            q1: {
+                question: "Which of these dinosaurs lived during the Jurassic Period?",
+                answers: {
+                a: "Triceratops",
+                b: "Stegosaurus",
+                c: "Eoraptor",
+                d: "Tyrannosaurus Rex",
+                },
+                correct: "b"
+            },
+            q2: {
+                question: "Which of the following is NOT a characteristic of reptiles?",
+                answers: {
+                a: "They are warmblooded",
+                b: "Most lay eggs",
+                c: "They have thick scaly skin",
+                d: "They breathe with lungs",
+                },
+                correct: "a"
+            },
+            q3: {
+                question: "Which of these mammals lay eggs?",
+                answers: {
+                a: "Echidna",
+                b: "Bongo",
+                c: "Kiwi",
+                d: "Southern right whale",
+                },
+                correct: "a"
+            },
+            q4: {
+                question: "What is the term for a young horse?",
+                answers: {
+                a: "Cub",
+                b: "Cygnet",
+                c: "Foal",
+                d: "Calf",
+                },
+                correct: "c"
+            },
+            q4: {
+                question: "What is the fastest animal on Earth?",
+                answers: {
+                a: "Cheetah",
+                b: "Peregrine Falcon",
+                c: "Leopard",
+                d: "Hyena",
+                },
+                correct: "b"
+            },
+            q5: {
+                question: "Which of these animals is NOT endangered?",
+                answers: {
+                a: "Red wolf",
+                b: "Sperm Whale",
+                c: "Capybara",
+                d: "Jaguar",
+                },
+                correct: "c"
+            },
+            q6: {
+                question: "Which animal could not survive in Africa?",
+                answers: {
+                a: "Hippopotamus",
+                b: "Tsetse fly",
+                c: "Wildcat",
+                d: "Giraffe",
+                },
+                correct: "c"
+            },
+            q7: {
+                question: "What is the most poisonous snake on the planet?",
+                answers: {
+                a: "Cobra",
+                b: "Inland Taipan",
+                c: "Western diamondback Rattlesnake",
+                d: "Black mamba",
+                },
+                correct: "b"
+            },
+            q8: {
+                question: "Which is the biggest species of shark?",
+                answers: {
+                a: "Great White shark",
+                b: "Hammerhead shark",
+                c: "Whale shark",
+                d: "Basking shark",
+                },
+                correct: "c"
+            },
+            q9: {
+                question: "What is a distinguishing feature of a marsupial?",
+                answers: {
+                a: "They change color to blend in with their environment",
+                b: "They live most of their lives in trees",
+                c: "They have very long necks",
+                d: "They carry their young in an external pouch",
+                },
+                correct: "d"
+            },
+        },
+        geography: {
+
+        },
+        movies: {
+            q1: {
+                question: "What was the top-grossing film for 2002?",
+                answers: {
+                a: "Signs",
+                b: "Star Wars: Episode II",
+                c: "My Big Fat Greek Wedding",
+                d: "Spiderman",
+                },
+                correct: "d"
+            },
+            q2: {
+                question: "Which 2005 film's tagline is \"Better Late Than Never\"?",
+                answers: {
+                a: "Fantastic Four",
+                b: "The Hitchhiker's Guide to the Galaxy",
+                c: "The Ring Two",
+                d: "The 40 Year Old Virgin",
+                },
+                correct: "d"
+            },
+        },
+        music: {
+
+        },
+        sports: {
+
+        }
+    }
+}
+
+
+/*
+//////////////////////////////////////////////////
+FORMAT FOR ADDING QUESTIONS:
+
+var questions = {
+    category: {
+        animals: {
+            q#: {
+                question: <YOUR QUESTION HERE>,
+                answers: {
+                    a: <answer A>,
+                    b: <answer B>,
+                    c: <answer C>,
+                    d: <answer D>,
+                },
+                correct: <THE CORRECT ANSWER LETTER>
+                }
+            }
+        }
+    }
+}
+//////////////////////////////////////////////////
+*/
