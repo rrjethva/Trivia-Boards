dccb7c2f5e22bf567ac98a3eb18eea8d309e8dd2
Ravi Jethva <rrjethva@gmail.com>
2019-12-01 20:37:49 -0500

    hopefully full model reversion

--- models/score.js
+++ models/score.js
@@ -1,10 +1,6 @@
 module.exports = function (sequelize, DataTypes) {
     var Score = sequelize.define("Score", {
         email: DataTypes.STRING,
-        user_id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-        },
         answered: DataTypes.INTEGER,
         correct: DataTypes.INTEGER,
         totalScore: DataTypes.INTEGER
